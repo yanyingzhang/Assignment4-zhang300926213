9a8c48547240589c0e19e88695583c569e2c40cf
Angelaaa <Angelaaa@LAPTOP-0Q8KPPTN>
2017-07-11 20:14:37 -0400

    Create the main Program Class

--- Assignment4-zhang300926213/Program.cs
+++ Assignment4-zhang300926213/Program.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Name: Yanying Zhang - 300926213
+ * Date: July 11, 2017
+ * Description: COMP123 - Assignment 4
+ * Version: 0.1 - Create the main Program Class
+ */
 namespace Assignment4_zhang300926213
 {
     class Program
