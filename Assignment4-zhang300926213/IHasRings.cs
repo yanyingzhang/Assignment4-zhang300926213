b80dd0bf3c9eb843ad3681f6600a80a3b4133738
Angelaaa <Angelaaa@LAPTOP-0Q8KPPTN>
2017-07-15 00:48:34 -0400

    Create the IHasRings interface

--- Assignment4-zhang300926213/IHasRings.cs
+++ Assignment4-zhang300926213/IHasRings.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+/*
+ * Name: Yanying Zhang - 300926213
+ * Date: July 13, 2017
+ * Description: COMP123 - Assignment 4
+ * Version: 0.1 - Create the IHasRings interface
+ */
 namespace Assignment4_zhang300926213
 {
     public interface IHasRings
     {
+        bool HasRings { get; set; }
     }
 }
