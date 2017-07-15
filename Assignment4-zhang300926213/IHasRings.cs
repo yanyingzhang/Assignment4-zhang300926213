c5960dcb90b3d0ce312821bd620a6e9ad05213ce
Angelaaa <Angelaaa@LAPTOP-0Q8KPPTN>
2017-07-15 00:47:22 -0400

    Create the IHasMoon interface

---
+++ Assignment4-zhang300926213/IHasRings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment4_zhang300926213
+{
+    public interface IHasRings
+    {
+    }
+}
