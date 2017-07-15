7d7500049b82339cb292c74851a48c7d6be14db3
Angelaaa <Angelaaa@LAPTOP-0Q8KPPTN>
2017-07-15 00:53:20 -0400

    Create the IHabitable interface

---
+++ Assignment4-zhang300926213/IHabitable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Yanying Zhang - 300926213
+ * Date: July 13, 2017
+ * Description: COMP123 - Assignment 4
+ * Version: 0.1 - Create the IHabitable interface
+ */
+namespace Assignment4_zhang300926213
+{
+    public interface IHabitable
+    {
+        bool Habitable { get; set; }
+    }
+}
