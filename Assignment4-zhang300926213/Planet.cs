2f04c9530a4578716852be5c12a0e458503fafb8
Angelaaa <Angelaaa@LAPTOP-0Q8KPPTN>
2017-07-26 18:58:11 -0400

    Modified the public propertites

--- Assignment4-zhang300926213/Planet.cs
+++ Assignment4-zhang300926213/Planet.cs
@@ -7,7 +7,7 @@ using System.Text;
  * Name: Yanying Zhang - 300926213
  * Date: July 11, 2017
  * Description: COMP123 - Assignment 4
- * Version: 0.1 - Create the abstract Planet Class
+ * Version: 0.2 - Modified the public propertites
  */
 namespace Assignment4_zhang300926213
 {
@@ -23,23 +23,81 @@ namespace Assignment4_zhang300926213
         private double _rotationPeriod;
 
         // PUBLIC PROPERTITES - 7 properties
-        public double Diameter { get; }
-        public double Mass { get; }
-        public int MoonCount { get; set; }
-        public string Name { get; }
-        public double OrbitalPeriod { get; set; }
-        public int RingCount { get; set; }
-        public double RotationPeriod { get; set; }
-        
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+            }
+        }
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+            }
+        }
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                this._moonCount = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+        }
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+        public double RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+
         // CONSTRUCTOR
 
-            /// <summary>
-            /// This is the constructor for the Planet class
-            /// It take three parameters - name(string), diameter(double), mass(double)
-            /// </summary>
-            /// <param name="name"></param>
-            /// <param name="diameter"></param>
-            /// <param name="mass"></param>
+        /// <summary>
+        /// This is the constructor for the Planet class
+        /// It take three parameters - name(string), diameter(double), mass(double)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
         public Planet(string name,double diameter, double mass)
         {
             this._name = name;
